393f7e958e9672bc8ee4af0dc56bed548f736604
oxford <baiysh.zmb@gmail.com>
2023-01-05 23:28:40 +0600

    added created methods

--- src/service/serviceImpl/ClientServiceImpl.java
+++ src/service/serviceImpl/ClientServiceImpl.java
@@ -2,20 +2,26 @@ package service.serviceImpl;
 
 import classes.Client;
 import classes.Taxi;
+import dao.Database;
 import service.ClientService;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ClientServiceImpl implements ClientService {
+    private Database database;
     @Override
     public String addClient(Client client) {
-        return null;
+        database.getClients().add(client);
+        return "successfully added client";
     }
 
     @Override
     public String addClient(List<Client> clients) {
-        return null;
+        database.getClients().addAll(clients);
+        return "successfully added clients";
     }
 
     @Override
@@ -40,6 +46,30 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public void universalSorting(String word) {
+        while (true) {
+            System.out.println("""
+                    press 1 to sorted A-Z
+                    press 2 to sorted Z-A
+                    press 3 to sorted date of birth ascending
+                    press 4 to sorted date of birth descending
+                    press 5 to sorted money ascending
+                    press 6 to sorted money descending""");
+            int str = new Scanner(System.in).nextInt();
+            switch (str) {
+                case 1 -> database.getClients().stream().sorted(Comparator.comparing(Client::getFullName)).forEach(System.out::println);
+                case 2 -> database.getClients().stream().sorted(Comparator.comparing(Client::getFullName).reversed()).forEach(System.out::println);
+                case 3 -> database.getClients().stream().sorted(Comparator.comparing(Client::getDateOfBirth)).forEach(System.out::println);
+                case 4 -> database.getClients().stream().sorted(Comparator.comparing(Client::getDateOfBirth).reversed()).forEach(System.out::println);
+                case 5 -> database.getClients().stream().sorted((Comparator.comparing(Client::getMoney))).forEach(System.out::println);
+                case 6 -> database.getClients().stream().sorted(Comparator.comparing(Client::getMoney).reversed()).forEach(System.out::println);
+                default -> System.out.println("error");
+            }
+            if (str == 9) {
+                break;
+
+            }
+        }
 
     }
+
 }
