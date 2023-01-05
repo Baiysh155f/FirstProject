679d0e77b0fa71d9eaeee2ad38ba48d8d51b196d
oxford <baiysh.zmb@gmail.com>
2023-01-05 23:37:32 +0600

    added created methods end main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import classes.Client;
+import service.serviceImpl.ClientServiceImpl;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        Client client = new Client(1L,"Baiysh Orozaliev", LocalDate.of(2000,1,20),"+996552930920", BigDecimal.valueOf(80000));
+        Client client1 = new Client(2L,"Davran Joldoshbaev", LocalDate.of(2004,4,12),"+996552930920", BigDecimal.valueOf(3000));
+        Client client2 = new Client(3L,"Eliza Ashyrbaeva", LocalDate.of(2003,3,5),"+996552930920", BigDecimal.valueOf(5900));
+
+        Client client3 = new Client(4L,"Kurstan Erkinbaev", LocalDate.of(2023,7,13),"+996552930920", BigDecimal.valueOf(25000));
+
+        Set<Client> clients = new HashSet<>(Arrays.asList(client,client1,client2));
+        System.out.println(clients);
+        ClientServiceImpl clientService = new ClientServiceImpl();
     }
 }
