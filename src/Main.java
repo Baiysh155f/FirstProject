4c3b86d3a556d4ab3c779907b40b260e846a3e86
Davran2005 <davranjoldoshbaev2@gmail.com>
2023-01-06 10:57:15 +0600

    Added enum ta

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-import classes.Taxi;
-import enums.TaxiType;
-import service.serviceImpl.TaxiServiceImpl;
+import classes.Client;
+import service.serviceImpl.ClientServiceImpl;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Main  {static Scanner scanner = new Scanner(System.in);
+public class Main {
     public static void main(String[] args) {
+        Client client = new Client(1L,"Baiysh Orozaliev", LocalDate.of(2000,1,20),"+996552930920", BigDecimal.valueOf(80000));
+        Client client1 = new Client(2L,"Davran Joldoshbaev", LocalDate.of(2004,4,12),"+996552930920", BigDecimal.valueOf(3000));
+        Client client2 = new Client(3L,"Eliza Ashyrbaeva", LocalDate.of(2003,3,5),"+996552930920", BigDecimal.valueOf(5900));
 
+        Client client3 = new Client(4L,"Kurstan Erkinbaev", LocalDate.of(2023,7,13),"+996552930920", BigDecimal.valueOf(25000));
 
+        Set<Client> clients = new HashSet<>(Arrays.asList(client, client1, client2));
+        System.out.println(clients);
+//        ClientServiceImpl clientService = new ClientServiceImpl();
+//        System.out.println("______________________");
+//        System.out.println(clientService.addClient(client3));
     }
 }
--- src/enums/TaxiType.java
+++ src/enums/TaxiType.java
@@ -1,6 +1,8 @@
 package enums;
 
 import java.math.BigDecimal;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public enum TaxiType {
